c9019d7738319a0260aaf0adab25a27d165ca939
ajithpopo <gsajith@amaljyothi.ac.in>
2020-11-12 12:56:00 +0530

    Updates

---
+++ precache-manifest.8a5003446768e8cb6395340c99ae9725.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fc7ef8fd7331b89c8ba5cf7adecda6b",
+    "url": "/React-app/index.html"
+  },
+  {
+    "revision": "bf4c6e6d3f3bfc0064ba",
+    "url": "/React-app/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "edad837904805282cb30",
+    "url": "/React-app/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "bf4c6e6d3f3bfc0064ba",
+    "url": "/React-app/static/js/2.a86f804a.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/React-app/static/js/2.a86f804a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "edad837904805282cb30",
+    "url": "/React-app/static/js/main.1192ae2b.chunk.js"
+  },
+  {
+    "revision": "638738765b248673ef7b",
+    "url": "/React-app/static/js/runtime-main.fc014e74.js"
+  }
+]);
